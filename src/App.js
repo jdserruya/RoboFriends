e9c5834443f87676b64c52728061f2c9aa1b500e
James <jdserruya@gmail.com>
2018-08-17 17:07:27 +0100

    Uploading initial files

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { Robots } from './Robots';
+import './App.css';
+
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            Robots: Robots,
+            searchfield: ''
+        }
+    }
+
+onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+}
+
+render() {
+    const filteredRobots = this.state.Robots.filter(Robots => {
+        return Robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList Robots={filteredRobots}/>
+        </div>
+        );
+    }   
+}
+
+export default App;
