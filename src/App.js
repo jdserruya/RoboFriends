8f453018d7a2c84536b9ef30b75972119c513398
James <jdserruya@gmail.com>
2018-08-18 12:18:05 +0100

    Implemented scrolling
    Can now scroll without losing sight of the search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -36,7 +37,9 @@ render() {
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList Robots={filteredRobots}/>
+                <Scroll>
+                    <CardList Robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
         }
