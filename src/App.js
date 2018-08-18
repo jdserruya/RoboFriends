e9352798156e03f1ec2ae16e2f81c27a2549fa13
James <jdserruya@gmail.com>
2018-08-18 11:51:01 +0100

    Added API support
    User data is now loaded through an API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { Robots } from './Robots';
 import './App.css';
 
 
@@ -16,26 +15,35 @@ class App extends Component {
         }
     }
 
-    componentDidMount() {
-        console.log('check');
-    }
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ Robots: users }));
+}
 
 onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value})
+    this.setState({ searchfield: event.target.value })
 }
 
 render() {
     const filteredRobots = this.state.Robots.filter(Robots => {
         return Robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-        <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList Robots={filteredRobots}/>
-        </div>
+    if (this.state.Robots.length === 0) {
+        return <h1>Loading</h1>
+    }   else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList Robots={filteredRobots}/>
+            </div>
         );
-    }   
+        }
+    
+    }
 }
+
+
 
 export default App;
